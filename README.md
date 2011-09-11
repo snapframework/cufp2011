020e707b2c011c718b4e21d44dfe57ad0d49c1e4
Gregory Collins <greg@gregorycollins.net>
2011-09-11 14:18:38 +0200

    Write most of the README

---
+++ README.md
@@ -0,0 +1,218 @@
+# CUFP 2011 Tutorial: Web programming in Haskell with the Snap Framework
+
+This repository contains code for the CUFP 2011 tutorial
+[The Snap Framework for web applications in Haskell](http://cufp.org/conference/sessions/2011/t7-snap-framework-web-applications-haskell-gregory).
+You will be building a simplified web application implementing multi-user chat.
+
+
+## Getting started
+
+From the project root directory, running `cabal install` should build the
+`snap-chat` and `snap-chat-sample` executables. You can find these executables
+in the `dist/build/` subdirectory. To run the sample application (to see what
+the result should look like), run
+`dist/build/snap-chat-sample/snap-chat-sample` and point your browser to
+[`http://localhost:8000/`](http://localhost:8000/).
+
+The sample implementation of the code you're expected to provide can be found
+in the `sample-implementation/` directory; however **please don't peek** unless
+you get really stuck. We'll be working through the code together.
+
+Test code (so you can test your implementation against the expected result) can
+be found in the `test/` directory; to run it:
+
+    cd test
+    cabal install
+    ./runTestsAndCoverage.sh
+
+
+## Specifications
+
+Snap Chat is split into three functional components:
+
+  * a data model, consisting of Haskell code for interacting with a chat
+    channel; see `src/Snap/Chat/Types.hs`, `src/Snap/Chat/ChatRoom.hs`, and
+    `src/Snap/Chat/Message.hs` for the implementation here.
+
+  * a JSON API that provides access to the data model; this consists of HTTP
+    `POST` handlers for the URL endpoints:
+
+    * `/api/join`, to join the chat channel
+    * `/api/leave`, to leave the chat channel
+    * `/api/fetch`, to fetch new messages written to the chat channel (using
+      long-polling)
+    * `/api/write`, to write a message to the chat channel.
+
+    These API endpoints will be described in further detail later.
+
+  * an HTML5/Javascript front end that calls the JSON api.
+
+For brevity and to bound the amount of work students are expected to do in the
+short tutorial, most of the code is already provided, including all of the
+JavaScript (this is a Haskell tutorial!), the data model code, and all of the
+datatypes. More advanced students who quickly breeze through the small amount
+of work provided can check out the "extra credit" section at the end of this
+document.
+
+
+### API documentation
+
+Each of the four API calls (`/api/join`, `/api/leave`, `/api/fetch`,
+`/api/write`) share a similar structure: they all respond only to POST requests
+containing a UTF8-encoded JSON document as the request body; i.e. the
+`Content-Type` of the input request is `application/json`, and they all produce
+a JSON document as the result. The output responses have the following common
+structure: either they succeed, producing a document like the following:
+
+    {
+      "status": "ok",
+      "session": "DF1642....038A=",
+      "response": { ...some json object... }
+    }
+
+When they fail, the output document looks like this:
+
+    {
+      "status": "failure",
+      "statusCode": "some_failure",
+      "reason": "blah blah blah blah."
+    }
+
+The "session" variable above deserves some special mention: upon successfully
+joining the chat room, the user will receive an encrypted session token, which
+will be used on subsequent requests to re-authenticate the user with the chat
+room. Upon each response, a fresh session token will be generated. The contents
+of the session token are opaque to the API user, but can be decrypted on the
+server-side.
+
+The data type for the encoded session looks like this (see
+`src/Snap/Chat/Internal/API/Types.hs`):
+
+    data EncodedSession = EncodedSession {
+          _sessionToken :: UserToken
+        , _sessionTime  :: EpochTime
+        , _apiUser      :: UserName
+        }
+
+A session will only be considered valid if:
+
+  * the session time is not too old.
+
+  * the user name and user token match what is contained in the chat room data
+    model.
+
+
+#### `/api/join`
+
+The "join" command is responsible for connecting a user to the chat room with a
+given user name.
+
+Example request:
+
+    { "desiredUserName": "george" }
+
+Example successful response:
+
+    {
+      "status": "ok",
+      "session": "abc.....def",
+      "response": {}
+    }
+
+Example unsuccessful response:
+
+    {
+      "status": "failure",
+      "statusCode": "user_already_exists",
+      "reason": "Cannot log in; a user with that name is already connected \
+                 to the channel."
+    }
+
+
+#### `/api/leave`
+
+The "leave" command logs the user out of the chat room.
+
+Example request:
+
+    {
+      "session": "abc.....def",
+      "requestData": {}
+    }
+
+Example successful response:
+
+    {
+      "status": "ok",
+      "session": "abc.....def",
+      "response": {}
+    }
+
+#### `/api/fetch`
+
+The "fetch" command gets new messages from the chat room, blocking for up to 50
+seconds before it returns with a list of new messages, possibly empty.
+
+Example request:
+
+    {
+      "session": "abc.....def",
+      "requestData": {}
+    }
+
+Example successful response:
+
+    {
+      "status": "ok",
+      "session": "abc.....def",
+      "response": [ ...messages... ]
+    }
+
+The JSON type of messages is as follows:
+
+    { "contents": {
+        "type": <<one of "talk", "action", "join", "leave">>,
+        "text": "message text"
+        },
+      "user": "fred",
+      "time": <<posix timestamp>>
+    }
+
+
+#### `/api/write`
+
+The "write" command writes a message to the chat room.
+
+Example request:
+
+    {
+      "session": "abc.....def",
+      "requestData": {
+          "type": <<one of "talk", "action">>,
+          "text": "message text"
+        }
+    }
+
+Example successful response:
+
+    {
+      "status": "ok",
+      "session": "abc.....def",
+      "response": {}
+    }
+
+
+## What students must implement
+
+
+
+## Extra credit
+
+If you finish early and get bored, here are some ideas for "extra-credit"
+assignments:
+
+  * extend the chat channel with a user list
+
+  * add private user-to-user messages
+
+  * support multiple chat rooms
---
+++ src/Snap/Chat/API/Handlers.hs
@@ -0,0 +1,206 @@
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Snap.Chat.API.Handlers
+  ( apiHandlers
+  ) where
+
+------------------------------------------------------------------------------
+import           Control.Exception (SomeException)
+import           Control.Monad
+import           Control.Monad.CatchIO
+import           Control.Monad.Reader
+import           Data.Aeson
+import           Data.Aeson.Encode
+import           Data.Attoparsec hiding (try)
+import           Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as S
+import qualified Data.ByteString.Lazy.Char8 as L
+import           Data.Char
+import qualified Data.Text as T
+import           Prelude hiding (catch)
+import           Snap.Iteratee (($$), consume, joinI, takeNoMoreThan)
+import           Snap.Types
+import           System.PosixCompat.Time
+import           Web.ClientSession
+------------------------------------------------------------------------------
+import           Snap.Chat.ChatRoom
+import           Snap.Chat.Internal.Types
+import           Snap.Chat.Internal.API.Types
+
+
+------------------------------------------------------------------------------
+apiHandlers :: Key -> ChatRoom -> Snap ()
+apiHandlers key chatRoom =
+    flip runReaderT chatRoom $
+         route [ ("join",  apiCall $ handleJoin key             )
+               , ("leave", authenticatingApiCall key handleLeave)
+               , ("fetch", authenticatingApiCall key handleFetch)
+               , ("write", authenticatingApiCall key handleWrite)
+               ]
+
+
+------------------------------------------------------------------------------
+-- | Scaffold common to all api requests. Handles:
+--
+-- * ensuring that the request is a POST with the correct MIME type
+--   (application/json)
+--
+-- * decoding the request body
+--
+-- * running the user request
+--
+-- * if successful, encoding the response and sending it
+apiCall :: (FromJSON req, ToJSON resp) =>
+           (req -> ApiHandler (ApiResponse resp))
+        -> ApiHandler ()
+apiCall f = method POST $ do
+    -- Check that the content-type is JSON. Strip off any charset suffixes.
+    ct <- liftM (fmap (S.takeWhile (\c -> c /= ';' && not (isSpace c)))
+                      . getHeader "Content-Type") getRequest
+
+    when (ct /= Just "application/json") $
+         finishWith $ setResponseCode 415 emptyResponse
+
+    -- Grab the JSON request body
+    jsonInput <- fetchRequestBody
+
+    let parseResult = parseOnly json jsonInput
+    either errorOut
+           (\obj -> do
+                let input = fromJSON obj
+                case input of
+                  Error e   -> errorOut e
+                  Success a -> do
+                    output <- f a
+                    modifyResponse $ setContentType "application/json"
+                    writeBuilder $ fromValue $ toJSON output)
+           parseResult
+  where
+    maxSize = 131072    -- 128 kB should be enough for anybody
+
+    fetchRequestBody = liftM S.concat $ runRequestBody $
+                       joinI $ takeNoMoreThan maxSize $$ consume
+
+    errorOut e = do
+        putResponse emptyResponse
+        writeText $ "Error decoding JSON input:\n"
+        writeText $ T.pack $ show e
+        getResponse >>= finishWith . setResponseCode 415
+
+
+------------------------------------------------------------------------------
+-- | Scaffold common to api requests that require an authenticated user.
+-- Handles:
+--
+-- * Decoding the input session token
+--
+-- * Looking up that the user is connected
+--
+-- * Making sure the authentication token matches
+--
+-- * Running the user request
+--
+-- * Encoding the output session token
+authenticatingApiCall :: (HasStatus resp, FromJSON req, ToJSON resp) =>
+                         Key
+                      -> (User -> req -> ApiHandler resp)
+                      -> ApiHandler ()
+authenticatingApiCall key = apiCall . authenticate key
+
+
+------------------------------------------------------------------------------
+authenticate :: (FromJSON req, ToJSON resp, HasStatus resp) =>
+                Key
+             -> (User -> req -> ApiHandler resp)
+             -> ApiRequest req
+             -> ApiHandler (ApiResponse resp)
+authenticate key f apiRequest = do
+    maybe (return authenticationFailure)
+          (\txt -> either (const $ return authenticationFailure)
+                          (\obj -> do
+                               let input = fromJSON obj
+                               case input of
+                                 Error _      -> return authenticationFailure
+                                 Success sess -> auth sess)
+                          (parseOnly json txt))
+          mbDecryptedText
+
+  where
+    encodedSession  = _encodedSession apiRequest
+    requestData     = _requestData    apiRequest
+    mbDecryptedText = decrypt key encodedSession
+
+    auth (EncodedSession token oldTime userName) = do
+        chatRoom <- ask :: ApiHandler ChatRoom
+        now <- liftIO epochTime
+        if now - oldTime > toEnum (_userTimeout chatRoom)
+          then return authenticationFailure
+          else do
+            eUser    <- try $ liftIO $ authenticateUser userName token
+                                                        chatRoom
+            either (\(_::SomeException) -> return authenticationFailure)
+                   (\user -> do
+                        resp <- f user requestData
+                        newEncodedSession <- liftIO $ encodeSession key user
+                        if isFailure resp
+                          then return $
+                               ApiResponseFailure (failureCode resp)
+                                                  (failureReason resp)
+                          else return $
+                               ApiResponseSuccess newEncodedSession resp)
+                   eUser
+
+
+------------------------------------------------------------------------------
+encodeSession :: Key -> User -> IO ByteString
+encodeSession key (User name _ token _) = epochTime >>= newEncodedSession
+  where
+    newEncodedSession now = do
+        let newSession = EncodedSession token now name
+        encryptIO key $ S.concat $ L.toChunks $ encode newSession
+
+------------------------------------------------------------------------------
+handleJoin :: Key
+           -> JoinRequest
+            -> ApiHandler (ApiResponse JoinResponse)
+handleJoin key (JoinRequest userName) = do
+    (ask >>= joinUp) `catch` \(_ :: UserAlreadyConnectedException) -> do
+        return $ ApiResponseFailure (failureCode resp) (failureReason resp)
+  where
+    resp = JoinResponseUserAlreadyExists
+
+    joinUp chatRoom = do
+        user <- liftIO $ joinUser userName chatRoom
+        newEncodedSession <- liftIO $ encodeSession key user
+        return $ ApiResponseSuccess newEncodedSession JoinResponseOK
+
+
+------------------------------------------------------------------------------
+handleLeave :: User -> LeaveRequest -> ApiHandler LeaveResponse
+handleLeave user _ = do
+    ask >>= liftIO . disconnectUser userName disconnectionReason
+    return LeaveResponseOK
+  where
+    userName = _userName user
+    disconnectionReason = T.concat [ " has left the channel." ]
+
+
+------------------------------------------------------------------------------
+handleFetch :: User -> GetMessagesRequest -> ApiHandler GetMessagesResponse
+handleFetch user _ = do
+    setTimeout $ defaultTimeout + 10
+    msgs <- ask >>= liftIO . getMessages defaultTimeout user
+    return $ GetMessagesOK msgs
+
+
+------------------------------------------------------------------------------
+handleWrite :: User -> WriteMessageRequest -> ApiHandler WriteMessageResponse
+handleWrite user (WriteMessageRequest msg) = do
+    ask >>= liftIO . writeMessageContents msg user
+    return WriteMessageResponseOK
+
+
+------------------------------------------------------------------------------
+defaultTimeout :: Int
+defaultTimeout = 50
